029e95e5737fefcaa58554b67be205437787aeba
shooxep0daemon <danielmv@mail.ru>
2021-03-15 22:39:19 +0300

    test 0.8

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -21,7 +21,30 @@ resource "google_compute_firewall" "default" {
 
 // Define VM resource
 resource "google_compute_instance" "instance_with_ip" {
-    name         = "drone-vm"
+    name         = "build-vm"
+    machine_type = "e2-medium" // 2vCPU, 4GB RAM
+    zone         = "${var.zone}"
+
+    boot_disk {
+        initialize_params{
+            image = "ubuntu-os-cloud/ubuntu-2004-lts"
+        }
+    }
+
+    metadata = {
+        ssh-keys = "${var.ssh_username}:${file(var.ssh_pub_key_path)}"
+    }    
+    
+    network_interface {
+        network = "default"
+        access_config {
+            nat_ip = "${google_compute_address.static.address}"
+        }
+    }
+}
+
+resource "google_compute_instance" "instance_with_ip" {
+    name         = "prod-vm"
     machine_type = "e2-medium" // 2vCPU, 4GB RAM
     zone         = "${var.zone}"
 
