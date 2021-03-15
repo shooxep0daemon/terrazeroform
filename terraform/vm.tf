1b734e200030a887f965662250c0d1d4d5c0f7c7
shooxep0daemon <danielmv@mail.ru>
2021-03-15 23:11:36 +0300

    wait for ip

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -75,6 +75,7 @@ output "runmachineip" {
 }
 //create inventory
 resource "null_resource" "ansible_hosts_provisioner" {
+   depends_on = [google_compute_instance.instance_with_ip2]
   provisioner "local-exec" {
     interpreter = ["/bin/bash" ,"-c"]
     command = <<EOT
